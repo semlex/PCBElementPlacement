608d5178dfdbd2431ec51e109417c81b49bdf5d9
hispe <hisperr@mail.ru>
2024-05-22 23:12:47 +0300

    Добавлены unit тесты

---
+++ PCBElementPlacement.Tests/PCBPlscementTests.cs
@@ -0,0 +1,56 @@
+namespace PCBElementPlacement.Tests
+{
+    [TestClass]
+    public class PCBElementPlacemenTests
+    {
+        [TestMethod]
+        public void CanPlaceElement_x_51_width_50_false_expected()
+        {
+            PCB pcb = new PCB(50, 50);
+            CircuitElement elem = new CircuitElement(51, 50, 1);
+            elem.X = 0;
+            elem.Y = 0;
+
+            bool result = pcb.CanPlaceElement(elem);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanPlaceElement_x_50_width_51_true_expected()
+        {
+            PCB pcb = new PCB(51, 50);
+            CircuitElement elem = new CircuitElement(50, 50, 1);
+            elem.X = 0;
+            elem.Y = 0;
+
+            bool result = pcb.CanPlaceElement(elem);
+
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CanPlaceElement_y_51_height_50_false_expected()
+        {
+            PCB pcb = new PCB(50, 50);
+            CircuitElement elem = new CircuitElement(50, 51, 1);
+            elem.X = 0;
+            elem.Y = 0;
+
+            bool result = pcb.CanPlaceElement(elem);
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void CanPlaceElement_y_50_height_51_true_expected()
+        {
+            PCB pcb = new PCB(50, 51);
+            CircuitElement elem = new CircuitElement(50, 50, 1);
+            elem.X = 0;
+            elem.Y = 0;
+
+            bool result = pcb.CanPlaceElement(elem);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
